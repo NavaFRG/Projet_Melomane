a609fecdbe1b18b3259d0b5a825e03dc1a351f06
Etienne-COMBE <86329905+Etienne-COMBE@users.noreply.github.com>
2021-06-30 17:06:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ artists_columns = ['id_x','followers','genres','name','popularity']
 artists_ = sp.tuple_to_list(artists_tuple)
 artists = pd.DataFrame(artists_, columns = artists_columns)
 
-
 chansons_tuple = sp.connection("SELECT * FROM chansons", "projetspotify.db")
 
 chansons_columns = ['id_y','name','popularity','duration_ms','explicit','artists'
@@ -33,6 +32,9 @@ chansons['year'] = chansons['release_date'].dt.year
 rows = chansons.index[index_]
 chansons.drop(rows, inplace=True)
 
+fusion = artists.merge(chansons, how="left", left_on=("id_x"), right_on=("id_artists"))
+fusion_cleaned = fusion.dropna(axis = 0, how = 'any')
+
 def danceability_by_tempo(chansons):
     danceability_by_tempo = chansons.groupby('tempo')['danceability'].sum()
     print(danceability_by_tempo)
@@ -45,7 +47,7 @@ def plot_danceability_over_year(chansons):
 
 def artists_by_year(chansons_bis):
     artists_by_year = chansons_bis.groupby('year')['artists'].nunique()
-    artists_by_year.plot()
+    artists_by_year.plot(title='Artists over time')
     
 def plots(chansons_bis):
     year_avg = chansons_bis[['danceability'
@@ -74,4 +76,11 @@ def plots(chansons_bis):
     plt.figure()
     sb.heatmap(corr, cmap='YlGnBu', annot=True)
 
-fusion = artists.merge(chansons, how="left", left_on=("id_x"), right_on=("id_artists"))
+def exploration (chansons):
+    print(chansons['duration_ms'].mean())
+    time_by_year = chansons.groupby('popularity')['duration_ms'].mean()
+    time_by_year.plot()
+    plt.figure()
+    time_by_year = chansons.groupby('popularity')['energy'].mean()
+    time_by_year.plot()
+
